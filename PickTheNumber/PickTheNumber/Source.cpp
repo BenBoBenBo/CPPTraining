fcfd8010926cc0310a84a3818a0cd5c3283015d3
Ben Bourland <ben@bourlandweb.com>
2018-03-28 22:03:09 -0700

    Removed the outer while loop from PickTheNumber

--- PickTheNumber/PickTheNumber/Source.cpp
+++ PickTheNumber/PickTheNumber/Source.cpp
@@ -11,50 +11,36 @@ int main()
 	int secretNumber = 2;
 	int userGuess;
 	int numberGuesses;
-	bool stillTrying;
-	int keepPlaying = 1;
+	int userQuit = 1;
+	bool stillTrying = true;
 
-	while (keepPlaying)
+	cout << "I have picked an integer between 0 and 10 - Can you guess it?" << endl;
+	cout << "Your guess: ";
+	cin >> userGuess;
+	numberGuesses = 1;
+
+	while (stillTrying)
 	{
-		cout << "I have picked an integer between 0 and 10 - Can you guess it?" << endl;
-		cout << "Your guess: ";
-		cin >> userGuess;
-		numberGuesses = 1;
-		stillTrying = true;
+		if (userGuess == secretNumber)
+		{
+			cout << "Great job!  You got it in " << numberGuesses << " guesses!! The number was " << secretNumber << "." << endl;
+			stillTrying = false;
+		}			
+		if (userGuess > secretNumber)
+		{
+			cout << "Ooops sorry, your guess is too high.  Try again!" << endl;
+			cout << "Your next guess: ";
+			cin >> userGuess;
+			numberGuesses++;
+		}
+		if (userGuess < secretNumber)
+		{
+			cout << "Ooops sorry, your guess is too low.  Try again!" << endl;
+			cout << "Your next guess: ";
+			cin >> userGuess;
+			numberGuesses++;
+		}
 
-		while (stillTrying)
-		{
-			if (userGuess == secretNumber)
-			{
-				cout << "Great job!  You got it in " << numberGuesses << " guesses!! The number was " << secretNumber << "." << endl;
-				stillTrying = false;
-			}			
-			if (userGuess > secretNumber)
-			{
-				cout << "Ooops sorry, your guess is too high.  Try again!" << endl;
-				cout << "Your next guess: ";
-				cin >> userGuess;
-				numberGuesses++;
-			}
-			if (userGuess < secretNumber)
-			{
-				cout << "Ooops sorry, your guess is too low.  Try again!" << endl;
-				cout << "Your next guess: ";
-				cin >> userGuess;
-				numberGuesses++;
-			}
-
-		}
-		cout << "Do you want to keep playing? (Press 0 to Quit):" << endl;
-		cin >> keepPlaying;
-		if (keepPlaying == 0)
-		{
-			keepPlaying = false;
-		}
-		if (keepPlaying != 0)
-		{
-			keepPlaying = true;
-		}
 	}
 	return 0;
 }
