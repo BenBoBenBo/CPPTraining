b8248e8a59f3646c413538de6e0ce79bbd64daaa
Ben Bourland <benbo@microsoft.com>
2018-03-29 09:28:27 -0700

    Updated a couple of text responses to PickTheNumber

--- PickTheNumber/PickTheNumber/Source.cpp
+++ PickTheNumber/PickTheNumber/Source.cpp
@@ -1,4 +1,4 @@
-// PickANumber.cpp : Learning exercise for CPP Training.
+// PickTheNumber.cpp : Learning exercise for CPP Training.
 // Author: Ben Bourland
 // Date: March 28, 2018
 
@@ -23,12 +23,21 @@ int main()
 	{
 		if (userGuess == secretNumber)
 		{
-			cout << "Great job!  You got it in " << numberGuesses << " guesses!! The number was " << secretNumber << "." << endl;
+			cout << "Great job!  You got Secret number in " << numberGuesses << " guesses!!" << endl;
 			stillTrying = false;
+			cout << "Press any key+ENTER to quit:";
+			cin >> userGuess;
 		}			
 		if (userGuess > secretNumber)
 		{
-			cout << "Ooops sorry, your guess is too high.  Try again!" << endl;
+			if (userGuess > 10)
+			{
+				cout << "Way too high, your guess needs to be between 0 and 10.  Try again!" << endl;
+			}
+			else
+			{
+				cout << "Ooops sorry, your guess is too high.  Try again!" << endl;
+			}
 			cout << "Your next guess: ";
 			cin >> userGuess;
 			numberGuesses++;
@@ -40,7 +49,6 @@ int main()
 			cin >> userGuess;
 			numberGuesses++;
 		}
-
 	}
 	return 0;
 }
